ff738aaee4f22bb0673eea13264c9d6bab065757
Flávio Carvalho <flaviocfr@gmail.com>
2019-10-11 11:05:51 -0300

    Criando as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "cursos" },
+  {
+    path: "cursos",
+    loadChildren: "./cursos/cursos.module#CursosModule"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
