5aa91328fb6673407e1b670f682ad40a86a360fb
Flávio Carvalho <flaviocfr@gmail.com>
2019-10-11 12:48:39 -0300

    Http GET: listar registros

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CursosService } from "./cursos.service";
+import { Curso } from "../curso";
 
 @Component({
-  selector: 'app-cursos-lista',
-  templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.css']
+  selector: "app-cursos-lista",
+  templateUrl: "./cursos-lista.component.html",
+  styleUrls: ["./cursos-lista.component.css"],
+  preserveWhitespaces: true
 })
 export class CursosListaComponent implements OnInit {
+  cursos: Curso[];
 
-  constructor() { }
+  constructor(private service: CursosService) {}
 
   ngOnInit() {
+    this.service.list().subscribe(dados => (this.cursos = dados));
   }
-
 }
